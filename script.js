2ec1cc5e1d86685a9b0ef5aa899259f1e7a1dcc3
Lopon Thinley Wangchuk <thinleywangchuk478@gmail.com>
2026-07-16 18:01:10 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ---------- Constants ----------
 // PASTE YOUR APPS SCRIPT WEB APP URL HERE (from Deploy > New deployment)
-const BACKEND_URL = "https://script.google.com/macros/s/AKfycbyt9KHZ4FWIjaK0UqxkD_YW-yMCFqbikOXYCwzGi0khjumj5rpdM_cHD5sijwW9xHz53Q/exec";
+const BACKEND_URL = "https://script.google.com/macros/s/AKfycbwHGK41o0luNY77PDSxbQE8Rb-DxDh_mxnSjnojl4CcADNBXaYwWBbMFUZMKordzGcSfQ/exec";
 
 const CATEGORY_LABEL = { lessonPlan: "Lesson Plan", otherDocuments: "Other Document" };
 
