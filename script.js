1f6da3bdec040f667ce3106f1e3847180d80b783
Lopon Thinley Wangchuk <thinleywangchuk478@gmail.com>
2026-07-26 16:05:46 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ---------- Constants ----------
 // PASTE YOUR APPS SCRIPT WEB APP URL HERE (from Deploy > New deployment)
-const BACKEND_URL = "https://script.google.com/macros/s/AKfycbwg8Ewe8O2fyb_HN87zvKafdLiPRdMNCrwd2b6-2Q_3rQ1xYRKZCA4qWUDkHZIO4zlcTw/exec";
+const BACKEND_URL = "https://script.google.com/macros/s/AKfycbxgrHC9TYr6ghcCWDci3vZ9Al-dNhEvDMy-bnT9tVddEK4yVjBx-UdiG1UqgjnIOrCFDQ/exec";
 
 // Put your welcome sound file (e.g. "audio/welcome.mp3") in your project folder,
 // then update this path if needed. If the file is missing, playback just silently
